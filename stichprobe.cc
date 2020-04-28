bb0dfddd9511204b3509e167ef5b22876da9f474
daniellescow <lescowdaniel@gmail.com>
2020-04-28 16:56:32 +0000

    Aufgabe 2.1

--- stichprobe.cc
+++ stichprobe.cc
@@ -3,39 +3,34 @@
 #include<cmath>
 
 int main() {
-
-  int sum, c, i;
-  int const n = 234;
-  double mit, var, abw;
-
-  c = 0;
   std::ifstream fin("datensumme.txt");
-  for(i=0; i<n; ++i) {
+  int i;
+  double n = 234;
+  double c,b, sumc, mit, var, abw;
 
-    fin >> c;
-    sum += c;
-
-  }
   
-  mit = sum / n;
+  for(i=0; i<n; ++i) {
+    fin >> c;
+    sumc += c;
+  }
 
-  c= 0;
+  mit = sumc / n;
+  std::cout << "Mittelwert:  " << mit << std::endl;
+
+
   std::ifstream fin2("datensumme.txt");
   for(i=0; i<n; ++i) {
 
     fin2 >> c;
-    var += pow((c - mit), 2);
+    var += pow((c-mit), 2);
 
   }
 
   var /= n;
+  std::cout << "Varianz:  " << var << std::endl;
 
-  abw = sqrt(var);
+  std::cout << "Abweichung:  " <<sqrt(var) << std::endl;
 
   fin.close();
   fin2.close();
-
-  std::cout << "Mittelwert:  " << mit << std::endl;
-  std::cout << "Varianz:  " << var << std::endl;
-  std::cout << "Standardabweichung:  " << abw << std::endl;
 }
